8be3f4838812c5f967ac2f853e900fe17996f612
LeoPitBull <124892444+LeoPitBull@users.noreply.github.com>
2023-08-07 10:36:46 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,11 @@
+// Obtendo o botão pelo ID
+const botao = document.getElementById("meuBotao");
+
+// Adicionando um ouvinte de evento de clique ao botão
+botao.addEventListener("click", function() {
+  // Mensagem que será exibida após o clique
+  const mensagem = "QUE BOTADA BRABA!.";
+
+  // Exibindo a mensagem em um alerta
+  alert(mensagem);
+});
